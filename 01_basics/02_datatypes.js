8fff1fc2cd84600d6ca04a9a0e6dc8da00fff68e
guptaAakrati <155299166+guptaAakrati@users.noreply.github.com>
2025-01-13 09:57:55 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+ "use strict";   // treat all js code as newer version
+ 'use strice';
+
+//  alert(3+3);  we are using node.js not browser
+
+let age = 22;
+ let name = "nisha";
+let your_name_is_nisha = true;
+ let city;
+ let state = null;
+ let bigint = 12;
+let hometown = undefined;
+
+// console.log([age , name , city, state,hometown]);
+ console.log(age);
+
+
+// let age = 22;
+console.log(city);
+console.log(typeof(hometown));
+console.log(typeof(state));

affd0e0d8875d08999c99434223a2b8c2f87f4b8
guptaAakrati <155299166+guptaAakrati@users.noreply.github.com>
2025-01-22 06:45:00 +0000

    conversion-of-datatypes

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,71 @@
+//  number
+
+let score = 33;
+
+console.log(score);
+console.log(typeof score);
+console.log(typeof (score));
+
+
+
+//  string
+
+let score1 = '33';
+console.log(typeof score1);
+
+let score2 = '33avc';
+console.log(typeof score2);
+
+let valueInNumber = Number(score2);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(score2);
+
+//  null
+let score3 = 'null';
+console.log(typeof score2);
+
+let valueInNumbers = Number(score3);
+console.log(typeof valueInNumbers);
+console.log(valueInNumbers);
+console.log(score3);
+
+
+//  undefined
+let score4 = 'undefined';
+console.log(typeof score2);
+
+let valueInNumber2 = Number(score4);
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+console.log(score4);
+
+//  boolean
+let score5 = true;
+console.log(typeof score5);
+
+let valueInNumber5 = Number(score5);
+console.log(typeof valueInNumber5);
+console.log(valueInNumber5);
+console.log(score5);
+
+//  33 => "33"
+//  '33abc' => NaN ; Typef NaN = number
+//  true => 1
+
+
+
+//  1 = true
+//  'nisha' = ture
+//  '' = false
+
+let boolean = 1;
+let boolean1 = 'nisha'
+
+let isboolean = Boolean(boolean);
+console.log(typeof isboolean);
+console.log(boolean);
+
+let isboolean1 = Boolean(boolean1);
+console.log(typeof isboolean1);
+console.log(boolean1);

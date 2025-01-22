73b709f5af389054912eec52abaebef5459708f0
guptaAakrati <155299166+guptaAakrati@users.noreply.github.com>
2025-01-22 07:06:25 +0000

    operations-conversion

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -2,52 +2,52 @@
 
 let score = 33;
 
-console.log(score);
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof (score));
 
 
 
 //  string
 
 let score1 = '33';
-console.log(typeof score1);
+// console.log(typeof score1);
 
 let score2 = '33avc';
-console.log(typeof score2);
+// console.log(typeof score2);
 
 let valueInNumber = Number(score2);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-console.log(score2);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(score2);
 
 //  null
 let score3 = 'null';
-console.log(typeof score2);
+// console.log(typeof score2);
 
 let valueInNumbers = Number(score3);
-console.log(typeof valueInNumbers);
-console.log(valueInNumbers);
-console.log(score3);
+// console.log(typeof valueInNumbers);
+// console.log(valueInNumbers);
+// console.log(score3);
 
 
 //  undefined
 let score4 = 'undefined';
-console.log(typeof score2);
+// console.log(typeof score2);
 
 let valueInNumber2 = Number(score4);
-console.log(typeof valueInNumber2);
-console.log(valueInNumber2);
-console.log(score4);
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
+// console.log(score4);
 
 //  boolean
 let score5 = true;
-console.log(typeof score5);
+// console.log(typeof score5);
 
 let valueInNumber5 = Number(score5);
-console.log(typeof valueInNumber5);
-console.log(valueInNumber5);
-console.log(score5);
+// console.log(typeof valueInNumber5);
+// console.log(valueInNumber5);
+// console.log(score5);
 
 //  33 => "33"
 //  '33abc' => NaN ; Typef NaN = number
@@ -63,9 +63,39 @@ let boolean = 1;
 let boolean1 = 'nisha'
 
 let isboolean = Boolean(boolean);
-console.log(typeof isboolean);
-console.log(boolean);
+// console.log(typeof isboolean);
+// console.log(boolean);
 
 let isboolean1 = Boolean(boolean1);
-console.log(typeof isboolean1);
-console.log(boolean1);
+// console.log(typeof isboolean1);
+// console.log(boolean1);
+
+
+
+
+// **********************************  Operations *********************************
+
+let value =2;
+let negvalue = -value;
+// console.log(negvalue);
+// console.log(value);
+
+// console.log(1+2);
+// console.log(4-2);
+// console.log(1*2);
+// console.log(4/2);
+// console.log(5%2);
+// console.log(2**3);
+// console.log(1+2);
+
+
+let str = 'hello';
+let str2 = ' nisha';
+let str3 =(str + str2);
+//  console.log(str3);
+
+ console.log('1' +2);
+ console.log(1 +'2');
+ console.log('1' +2 +3);
+ console.log(1 +'2' +2);
+ console.log(1 +2 +'2');

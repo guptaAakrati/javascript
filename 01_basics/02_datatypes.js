55f6039ef842f2412011adddc8e00ef133590175
guptaAakrati <155299166+guptaAakrati@users.noreply.github.com>
2025-01-22 06:11:35 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -10,6 +10,20 @@ let your_name_is_nisha = true;
  let state = null;
  let bigint = 12;
 let hometown = undefined;
+// primitive data type
+// number ,
+//  null , = Standalone value
+// string ,
+//  symbol , = unique
+//  undefined ,
+//  bigint , 
+// int ,
+//  boolean . = True and False
+
+// non-primitive data type
+
+//    object
+
 
 // console.log([age , name , city, state,hometown]);
  console.log(age);
@@ -19,3 +33,5 @@ let hometown = undefined;
 console.log(city);
 console.log(typeof(hometown));
 console.log(typeof(state));
+
+console.log(typeof (null));

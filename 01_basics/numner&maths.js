2caeac1a4b729b98979dd15e36530b172911d147
guptaAakrati <155299166+guptaAakrati@users.noreply.github.com>
2025-01-29 11:30:03 +0000

    number&maths

---
+++ 01_basics/numner&maths.js
@@ -0,0 +1,48 @@
+const score = 200;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+const otherNum = 122.9897;
+// console.log(otherNum.toFixed(2));
+// console.log(otherNum.toFixed(1));
+
+
+// console.log(otherNum.toPrecision(3));
+
+const hunderds = 100000000;
+// console.log(hunderds.toLocaleString());
+// console.log(hunderds.toLocaleString('en-IN'))
+
+
+// ++++++++++++++++++++++   MATHS ++++++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.round(4.8));
+// console.log(Math.ceil(4.8));
+// console.log((Math.floor(4.8)));
+// console.log(Math.abs(-4));
+
+// console.log(Math.min(3,4,5,6));
+// console.log(Math.max(3,4,5,6));
+
+// console.log(Math.random());  ----------> ( givin number between 0 to 1 )
+// console.log(Math.random() *10 + 1);
+// console.log((Math.random() )*10 + 1);
+// console.log(Math.floor(Math.random() )*10 + 1);
+
+const min = 10;
+const max = 20;
+
+// Math.random() * (max-min +1) + min;
+
+console.log(Math.floor(Math.random() * (max-min+1) + min));
+
+
+
+
+
